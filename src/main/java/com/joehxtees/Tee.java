36f46d4503be725a715a1a079619ec49dda072a3
hendrixjoseph <hendixjoseph@aol.com>
2022-07-07 15:04:41 -0400

    should probably do more commits

---
+++ src/main/java/com/joehxtees/Tee.java
@@ -0,0 +1,56 @@
+package com.joehxtees;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import com.google.gson.Gson;
+
+public class Tee {
+	private static final Map<String, Integer> SAME_TEE_COUNT = new HashMap<>();
+
+	private final int slugId;
+
+	private final String title;
+	private final String slug;
+	private final String imageSrc;
+	private final String src;
+	private final List<String> bullets = new ArrayList<>();
+
+	public Tee(final String title, final String imageSrc, final String src, final List<String> bullets) {
+		this.title = title;
+		this.slug = title.toLowerCase().replace(' ' , '-').replaceAll("&.+?;|\\?","").replaceAll("-+", "-");
+		this.imageSrc = imageSrc;
+		this.src = src;
+		this.bullets.addAll(bullets);
+
+		this.slugId = SAME_TEE_COUNT.compute(this.slug, (k,v) -> v == null ? 0 : v + 1);
+	}
+
+	public String getTitle() {
+		return this.title;
+	}
+
+	public String getImageSrc() {
+		return this.imageSrc;
+	}
+
+	public String getSrc() {
+		return this.src;
+	}
+
+	public List<String> getBullets() {
+		return this.bullets;
+	}
+
+	public String getPath() {
+		final String slugId = this.slugId == 0 ? "" : this.slugId + "/";
+		return this.slug + "/" + slugId;
+	}
+
+	@Override
+	public String toString() {
+		return new Gson().toJson(this);
+	}
+}
---
+++ src/main/java/com/joehxtees/TeeProcessor.java
@@ -0,0 +1,221 @@
+package com.joehxtees;
+
+import java.awt.Color;
+import java.awt.Point;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.Reader;
+import java.io.Writer;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.Comparator;
+import java.util.List;
+import java.util.Map;
+import java.util.Stack;
+import java.util.stream.Collectors;
+
+import javax.imageio.ImageIO;
+
+import com.google.gson.Gson;
+
+public class TeeProcessor {
+	private static final String TITLE = "{{ title }}";
+	private static final String BULLETS = "{{ bullets }}";
+	private static final String IMAGE = "{{ image }}";
+	private static final String SRC = "{{ src }}";
+	private static final String CONTENT = "{{ content }}";
+	private static final String PATH = "{{ path }}";
+
+	private static final String SITE_DIR = "_site";
+	private static final String IMAGE_FILENAME = "image.jpg";
+
+	private final List<Tee> tees;
+	private final Map<Tee, String> teeHtmls;
+
+	private final String listTemplate;
+	private final String teeTemplate;
+	private final String detailTemplate;
+
+	public static void delete() throws IOException {
+		Files.walk(Path.of(SITE_DIR))
+			.sorted(Comparator.reverseOrder())
+			.forEach(t -> {
+			try {
+				Files.delete(t);
+			} catch (final IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		});
+	}
+
+	public TeeProcessor() throws IOException {
+		this.listTemplate = Files.readString(Path.of("list.template.html"));
+		this.teeTemplate = Files.readString(Path.of("shirt.template.html"));
+		this.detailTemplate = Files.readString(Path.of("detail.template.html"));
+
+		Files.createDirectories(Paths.get(SITE_DIR));
+
+		final Gson gson = new Gson();
+
+		final Reader reader = new FileReader("tees.json");
+
+		final List<List<?>> objects = gson.fromJson(reader, List.class);
+
+		this.tees = objects.stream().map(o -> {
+			final String title = o.get(0).toString();
+			final List<String> bullets = (List<String>) o.get(1);
+			final String imageSrc = o.get(2).toString();
+			final String src = o.get(3).toString();
+
+			return new Tee(title, imageSrc, src, bullets);
+		  })
+		  .collect(Collectors.toList());
+
+		this.teeHtmls = this.tees.stream().collect(
+			Collectors.toMap(
+				tee -> tee,
+				tee -> templatize(this.teeTemplate, tee)
+			)
+		);
+
+		reader.close();
+	}
+
+	public void createDetailPages() {
+		this.teeHtmls.entrySet().stream().forEach(entry -> {
+			final Tee tee = entry.getKey();
+			final String teeHtml = entry.getValue();
+
+			final String detailHtml = templatize(this.detailTemplate, tee)
+					.replace(CONTENT, teeHtml.replaceAll("##unless-list.+", "")
+					.replace(IMAGE, IMAGE_FILENAME));
+
+			final Path dir = Paths.get(SITE_DIR, tee.getPath());
+
+			try (Writer detail = new FileWriter(dir.toString() + "/index.html")) {
+				Files.createDirectories(dir);
+				detail.write(detailHtml);
+			} catch (final IOException e) {
+				e.printStackTrace();
+			}
+		});
+	}
+
+	public void createIndex() throws IOException {
+		final Writer index = new FileWriter(SITE_DIR + "/index.html");
+
+		index.write(
+		  this.listTemplate.replace(CONTENT,
+			this.teeHtmls.entrySet().stream().map(entry -> {
+				final Tee tee = entry.getKey();
+				final String teeHtml = entry.getValue();
+
+				 return teeHtml
+					 .replace("##unless-list", "")
+					 .replace(PATH, tee.getPath())
+					 .replace(IMAGE, tee.getPath() + IMAGE_FILENAME);
+			}).collect(Collectors.joining())
+		  )
+	    );
+
+		index.close();
+	}
+
+	public void createSiteMap() throws IOException {
+		final Writer sitemap = new FileWriter(SITE_DIR + "/sitemap.xml");
+
+		sitemap.write("<?xml version=\"1.0\" encoding=\"UTF-8\"?>");
+		sitemap.write("<urlset xmlns=\"http://www.sitemaps.org/schemas/sitemap/0.9\">");
+
+		final String lastModTag = "<lastmod>"
+				+ LocalDate.now().format(DateTimeFormatter.ISO_LOCAL_DATE)
+				+ "</lastmod>";
+
+		sitemap.write(
+			this.tees.stream()
+				.map(Tee::getPath)
+				.map(path -> "https://www.joehxblog.com/" + path)
+				.map(url -> "<loc>" + url + "</loc>")
+				.map(locTag -> locTag + lastModTag)
+				.map(tags -> "<url>" + tags + "</url>")
+				.collect(Collectors.joining())
+		);
+
+		sitemap.write("</urlset>");
+
+		sitemap.close();
+	}
+
+	public void copyStaticFiles() throws IOException {
+		Files.list(Path.of(""))
+			.filter(path -> path.toString().endsWith("css"))
+			.forEach(file -> {
+				try {
+					Files.copy(file, Path.of(SITE_DIR, file.toString()));
+				} catch (final IOException e) {
+					e.printStackTrace();
+				}
+			});
+	}
+
+	private String templatize(final String template, final Tee tee) {
+		return template
+				.replace(TITLE, tee.getTitle())
+				//.replace(IMAGE, tee.slug + ".jpg")
+				.replace(SRC, tee.getSrc())
+				.replace(BULLETS, tee.getBullets().stream().collect(Collectors.joining("</li><li>", "<li>", "</li>")));
+	}
+
+	private void downloadImage(final Tee tee) throws MalformedURLException, IOException {
+		final BufferedImage image = ImageIO.read(new URL(tee.getImageSrc()));
+
+		final BufferedImage newImage = eraseBackground(image);
+
+		ImageIO.write(newImage, "png", new File(SITE_DIR + "/" + tee.getPath() + "image.png"));
+	}
+
+
+	private BufferedImage eraseBackground(final BufferedImage image) {
+		final BufferedImage newImage = new BufferedImage(image.getWidth(), image.getHeight(), BufferedImage.TYPE_INT_ARGB);
+
+		newImage.setData(image.getData());
+
+		final Stack<Point> stack = new Stack<>();
+		stack.push(new Point(0,0));
+
+		while (!stack.isEmpty()) {
+			final Point point = stack.pop();
+
+			if (point.x >= 0 && point.x < newImage.getWidth()
+					&& point.y >= 0 && point.y < newImage.getHeight()) {
+
+				final Color color = new Color(newImage.getRGB(point.x, point.y), true);
+
+				final float brightness = Color.RGBtoHSB(color.getRed(), color.getGreen(), color.getBlue(), null)[2];
+
+				if (brightness > 0.75 && color.getAlpha() == 255) {
+					final int alpha = (int) (256.0 * brightness);
+					final Color newColor = new Color(color.getRed(), color.getGreen(), color.getBlue(), 256 - alpha);
+
+					newImage.setRGB(point.x, point.y, newColor.getRGB());
+
+					stack.push(new Point(point.x + 1, point.y    ));
+					stack.push(new Point(point.x    , point.y + 1));
+					stack.push(new Point(point.x - 1, point.y    ));
+					stack.push(new Point(point.x    , point.y - 1));
+				}
+			}
+		}
+
+		return newImage;
+	}
+}
